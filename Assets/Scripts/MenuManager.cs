a0d126f34250d008e7e70ab29677981f1784676a
Phyrocast <71868443+ComeSerlooten@users.noreply.github.com>
2026-03-09 22:41:47 +0100

    menu scene
    added menu scene
    - start button
    - menu script import
    - input field

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,34 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MenuManager : MonoBehaviour
+{
+    public TMP_InputField input;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        input.onValueChanged.AddListener(SetName);
+
+        //input.text = DataTransfer.instance.playerName;
+    }
+
+    public void SetName(string name)
+    {
+        //DataTransfer.instance.playerName = name;
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+        //DataTransfer.instance.Save();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
